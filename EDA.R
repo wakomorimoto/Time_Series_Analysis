b23209adb722dbe61256cedee760601899696bd9
wakomorimoto <63631230+wakomorimoto@users.noreply.github.com>
2022-04-18 21:28:17 -0700

    Create EDA.R

---
+++ EDA.R
@@ -0,0 +1,27 @@
+
+library(tidyverse)
+library(ggplot2)
+phoenix <- read.csv('phoenix.csv')
+
+#combine year, month, and date columns
+phoenix$time <- as.Date(with(phoenix, paste(year,month,day, sep="-")), "%Y-%m-%d")
+
+#NO2
+NO2 <- phoenix %>%
+  select('year','month','day','time','no2')
+ggplot(NO2, aes(x=time, y=no2)) + geom_line()
+
+#O3
+O3 <- phoenix %>%
+  select('year','month','day','time','o3')
+ggplot(O3, aes(x=time, y=o3)) + geom_line()
+
+#SO2
+so2 <- phoenix %>%
+  select('year','month','day','time','so2')
+ggplot(so2, aes(x=time, y=so2)) + geom_line()
+
+#CO
+co <- phoenix %>%
+  select('year','month','day','time','co')
+ggplot(co, aes(x=time, y=co)) + geom_line()
